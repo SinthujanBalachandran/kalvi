94648ab819746606bf0b898c2f054848a92bf309
SinthujanBalachandran <54698571+SinthujanBalachandran@users.noreply.github.com>
2021-11-14 07:19:41 +0530

    scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,12 @@ let swiper = new Swiper(".testimonial_container", {
         slideShadows: false,
     },
 })
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
